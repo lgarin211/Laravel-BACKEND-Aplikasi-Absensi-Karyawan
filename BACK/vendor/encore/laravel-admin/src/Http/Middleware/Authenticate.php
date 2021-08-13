619da8a588c6360d461963e0f6349230a8455474
lgarin211 <lagustinus211@gmail.com>
2021-08-13 11:55:46 +0700

    masuk

---
+++ BACK/vendor/encore/laravel-admin/src/Http/Middleware/Authenticate.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Encore\Admin\Http\Middleware;
+
+use Closure;
+use Encore\Admin\Facades\Admin;
+
+class Authenticate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        \config(['auth.defaults.guard' => 'admin']);
+
+        $redirectTo = admin_base_path(config('admin.auth.redirect_to', 'auth/login'));
+
+        if (Admin::guard()->guest() && !$this->shouldPassThrough($request)) {
+            return redirect()->to($redirectTo);
+        }
+
+        return $next($request);
+    }
+
+    /**
+     * Determine if the request has a URI that should pass through verification.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return bool
+     */
+    protected function shouldPassThrough($request)
+    {
+        $except = trim(admin_base_path('auth/login'), '/');
+
+        return $request->is($except);
+    }
+}
