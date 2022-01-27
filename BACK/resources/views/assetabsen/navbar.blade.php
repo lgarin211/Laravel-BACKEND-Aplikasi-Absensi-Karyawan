e3c347e9214a43ea853a5dc777c4f09c5690b818
lgarin211 <server@server.smkn4bogor.sch.id>
2022-01-27 16:12:20 +0000

    addkan terakhir malam ini

--- BACK/resources/views/assetabsen/navbar.blade.php
+++ BACK/resources/views/assetabsen/navbar.blade.php
@@ -14,10 +14,10 @@
   <i data-feather="calendar" data-feather-line="1" data-feather-size="21" data-feather-color="blue2-dark" data-feather-bg="blue2-fade-light"></i>
   <span>Not Present</span>
   </a>
-  <a href="#" onclick="alert('Halaman Dalam Tahap Pengembangan')">
+  {{-- <a href="#" onclick="alert('Halaman Dalam Tahap Pengembangan')">
   <i data-feather="calendar" data-feather-line="1" data-feather-size="21" data-feather-color="blue2-dark" data-feather-bg="blue2-fade-light"></i>
   <span>Pengajuan</span>
-  </a>
+  </a> --}}
   <a href="#" onclick="alert('Halaman Dalam Tahap Pengembangan')">
   <i data-feather="calendar" data-feather-line="1" data-feather-size="21" data-feather-color="blue2-dark" data-feather-bg="blue2-fade-light"></i>
   <span>Laporan</span>
