6cd74405ddb2d3414c189b57a1e6060ec4bb45a3
lgarin211 <server@server.smkn4bogor.sch.id>
2022-01-27 16:12:15 +0000

    addkan terakhir malam ini

--- BACK/resources/views/assetabsen/navbar.blade.php
+++ BACK/resources/views/assetabsen/navbar.blade.php
@@ -1,45 +1,46 @@
-<div class="header header-fixed header-auto-show header-logo-app">
-        <a href="{{url('/azure')}}/#" data-toggle-theme class="header-icon header-icon-2 show-on-theme-dark"><i class="fas fa-sun"></i></a>
-        <a href="{{url('/azure')}}/#" data-toggle-theme class="header-icon header-icon-2 show-on-theme-light"><i class="fas fa-moon"></i></a>
-</div>
-<div id="footer-bar" class="footer-bar-5">
+  <div class="header header-fixed header-auto-show header-logo-app">
+    <a href="{{url('/azure')}}/#" data-toggle-theme class="header-icon header-icon-2 show-on-theme-dark"><i class="fas fa-sun"></i></a>
+    <a href="{{url('/azure')}}/#" data-toggle-theme class="header-icon header-icon-2 show-on-theme-light"><i class="fas fa-moon"></i></a>
+  </div>
+  <div id="footer-bar" class="footer-bar-5">
 
+    {{-- @dump(Auth::user()->jabatan) --}}
   @if (Auth::user()->jabatan=="KEPALA SEKOLAH")
   <a href="{{url('/kepsek/pen')}}">
-    <i data-feather="calendar" data-feather-line="1" data-feather-size="21" data-feather-color="blue2-dark" data-feather-bg="blue2-fade-light"></i>
-    <span>Present</span>
+  <i data-feather="calendar" data-feather-line="1" data-feather-size="21" data-feather-color="blue2-dark" data-feather-bg="blue2-fade-light"></i>
+  <span>Present</span>
   </a>
   <a href="{{url('/kepsek/ten')}}">
-    <i data-feather="calendar" data-feather-line="1" data-feather-size="21" data-feather-color="blue2-dark" data-feather-bg="blue2-fade-light"></i>
-    <span>Not Present</span>
-  </a>
-    <a href="#" onclick="alert('Halaman Dalam Tahap Pengembangan')">
-    <i data-feather="calendar" data-feather-line="1" data-feather-size="21" data-feather-color="blue2-dark" data-feather-bg="blue2-fade-light"></i>
-    <span>Pengajuan</span>
+  <i data-feather="calendar" data-feather-line="1" data-feather-size="21" data-feather-color="blue2-dark" data-feather-bg="blue2-fade-light"></i>
+  <span>Not Present</span>
   </a>
   <a href="#" onclick="alert('Halaman Dalam Tahap Pengembangan')">
-    <i data-feather="calendar" data-feather-line="1" data-feather-size="21" data-feather-color="blue2-dark" data-feather-bg="blue2-fade-light"></i>
-    <span>Laporan</span>
+  <i data-feather="calendar" data-feather-line="1" data-feather-size="21" data-feather-color="blue2-dark" data-feather-bg="blue2-fade-light"></i>
+  <span>Pengajuan</span>
+  </a>
+  <a href="#" onclick="alert('Halaman Dalam Tahap Pengembangan')">
+  <i data-feather="calendar" data-feather-line="1" data-feather-size="21" data-feather-color="blue2-dark" data-feather-bg="blue2-fade-light"></i>
+  <span>Laporan</span>
   </a>
   @else
-  
+
   <a href="{{url('/menu2')}}">
-    <i data-feather="file" data-feather-line="1" data-feather-size="21" data-feather-color="blue2-dark" data-feather-bg="blue2-fade-light"></i>
-    <span>informasi</span>
+  <i data-feather="file" data-feather-line="1" data-feather-size="21" data-feather-color="blue2-dark" data-feather-bg="blue2-fade-light"></i>
+  <span>informasi</span>
   </a>
   <a href="{{url('/absen')}}" class="">
-    <i data-feather="home" data-feather-line="1" data-feather-size="21" data-feather-color="blue2-dark" data-feather-bg="blue2-fade-light"></i>
-    <span>Home</span>
+  <i data-feather="home" data-feather-line="1" data-feather-size="21" data-feather-color="blue2-dark" data-feather-bg="blue2-fade-light"></i>
+  <span>Home</span>
   </a>
 
   <a href="{{url('/menu1')}}">
-    <i data-feather="calendar" data-feather-line="1" data-feather-size="21" data-feather-color="blue2-dark" data-feather-bg="blue2-fade-light"></i>
-    <span>Pengajuan</span>
+  <i data-feather="calendar" data-feather-line="1" data-feather-size="21" data-feather-color="blue2-dark" data-feather-bg="blue2-fade-light"></i>
+  <span>Pengajuan</span>
   </a>
 
   @endif
   <a href="{{url('/setting')}}">
-    <i data-feather="settings" data-feather-line="1" data-feather-size="21" data-feather-color="gray2-dark" data-feather-bg="gray2-fade-light"></i>
-    <span>Settings</span>
+  <i data-feather="settings" data-feather-line="1" data-feather-size="21" data-feather-color="gray2-dark" data-feather-bg="gray2-fade-light"></i>
+  <span>Settings</span>
   </a>
-</div>
+  </div>
